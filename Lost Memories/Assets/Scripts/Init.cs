fd6139060b98aee3a312aac890d9813a29518ae9
Tomasz Gieorgijewski <cybergeorge2020@gmail.com>
2021-01-30 16:38:51 +0100

    Scene setup with drawn backgrounds and parallax. Hero moves and jumps on the ground.

---
+++ Lost Memories/Assets/Scripts/Init.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Init : MonoBehaviour
+{
+    [SerializeField]
+    private float _gravity;
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics2D.gravity = new Vector2(0f, _gravity);
+    }
+}
---
+++ Lost Memories/Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Vector3 _velocity;
+    [SerializeField]
+    private float _speed;
+    [SerializeField]
+    private CharacterController _controller;
+    int jumps;
+    [SerializeField]
+    private float _jumpVelocity;
+    private float _gravity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        _gravity = Physics2D.gravity.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //grounded = _controller.isGrounded;
+        _velocity.x = Input.GetAxis("Horizontal") * _speed;
+        if (_controller.isGrounded)
+        {
+            jumps = 0;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _velocity.y += _jumpVelocity;
+                jumps++;
+            }
+            if (_velocity.y < 0) _velocity.y = 0.0f;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && jumps < 2)
+        {
+            _velocity.y += _jumpVelocity;
+            jumps++;
+        }
+        _velocity.y -= _gravity * Time.deltaTime;
+        _controller.Move(new Vector3(_velocity.x, _velocity.y) * Time.deltaTime);
+        
+    }
+}
