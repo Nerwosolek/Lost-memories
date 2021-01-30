162cced2742f593af10eb6754ec1a42d3f84390a
Tomasz Gieorgijewski <cybergeorge2020@gmail.com>
2021-01-30 19:24:20 +0100

    First animator of the chracter

---
+++ Lost Memories/Assets/Scripts/Interaction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : Interactable
+{
+    public bool ReadyToInteract()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public string Remember()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public string Scan()
+    {
+        throw new System.NotImplementedException();
+    }
+}
